4caf2e619eac373f9501b53e972579a8d72ada90
jf8283062 <1006200300@qq.com>
2018-01-24 16:54:38 +0800

    微信验证

---
+++ WXProject/WXProjectWeb/Controllers/WXCheckController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WXProjectWeb.wcApi;
+
+namespace WXProjectWeb.Controllers
+{
+    public class WXCheckController : Controller
+    {
+        public string WeiXin()
+        {
+            // 微信加密签名
+            string signature = Request["SIGNATURE"];
+            // 时间戮
+            string timestamp = Request["TIMESTAMP"];
+            // 随机数
+            string nonce = Request["NONCE"];
+            // 随机字符串
+            string echostr = Request["echostr"];
+            return CommonBLL.CheckSignature(signature, timestamp, nonce) ? echostr : "";
+        }
+
+    }
+}
---
+++ WXProject/WXProjectWeb/wcApi/CommonBLL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace WXProjectWeb.wcApi
+{
+    public class CommonBLL
+    {
+        public readonly static string token = "jiaofeng";
+        public readonly static string APIkey = "bce5a7b4-a168-4ca1-9462-79462b739b38";
+        public readonly string APIsecrit = "76b16982-af5a-448a-9939-106a01488cde";
+        /// <summary>
+        /// 加密验证
+        /// </summary>
+        /// <param name="signature">源</param>
+        /// <param name="timestamp">时间戳</param>
+        /// <param name="nonce">nonce</param>
+        /// <param name="token">自定义Token</param>
+        /// <returns></returns>
+        public static bool CheckSignature(string signature, string timestamp, string nonce)
+        {
+            List<string> list = new List<string>();
+            list.Add(token);
+            list.Add(timestamp);
+            list.Add(nonce);
+            list.Sort();
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in list)
+            {
+                sb.Append(item);
+            }
+            System.Security.Cryptography.HashAlgorithm SHA_1 = new System.Security.Cryptography.SHA1CryptoServiceProvider();
+            byte[] str = Encoding.UTF8.GetBytes(sb.ToString());
+            str = SHA_1.ComputeHash(str);
+            StringBuilder strSB = new StringBuilder();
+            foreach (var item in str)
+            {
+                strSB.AppendFormat("{0:x2}", item);
+            }
+            return strSB.ToString() == signature;
+
+        }
+    }
+}
