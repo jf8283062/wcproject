f9929b07d07d8aeb406a4866a1491e7ea70256b4
jf8283062 <1006200300@qq.com>
2018-01-31 17:52:02 +0800

    1

--- WXProject/WXProjectWeb/Controllers/HomeController.cs
+++ WXProject/WXProjectWeb/Controllers/HomeController.cs
@@ -17,49 +17,84 @@ namespace WXProjectWeb.Controllers
 {
     public class HomeController : Controller
     {
+        public static Dictionary<string, DateTime> Access_token = null;
         /// <summary>
         /// sssss
         /// </summary>
         /// <returns></returns>
         public ActionResult Index()
         {
+            if (Access_token == null || Access_token.FirstOrDefault().Value < DateTime.Now)
+            {
+                var token = CommonBLL.GetAccess_token();
+                Access_token = new Dictionary<string, DateTime>() {
+                    { token, DateTime.Now.AddSeconds(7000) }
+                    };
+            }
             StreamReader sr = new StreamReader(Request.InputStream, Encoding.UTF8);
             string text = sr.ReadToEnd();
             if (!string.IsNullOrEmpty(text))
             {
+                string resStr = "";
                 var eventmodel = WXMethdBLL.CreateMessage(text);
                 if (eventmodel != null && eventmodel is SubscribeEvent)
                 {
                     SubscribeEvent model = eventmodel as SubscribeEvent;
-                    var resStr = WXMethdBLL.ResponseMsg(new Modal.WeiXinRequest.ContentRequest()
+                    if (string.IsNullOrWhiteSpace(model.EventKey))
+                    {
+                        var user = UserBLL.GetUserInfo(model.EventKey);
+                        UserBLL.UpdateUser(user);
+                    }
+                    else
+                    {
+                        var user = UserBLL.GetUserDetail(Access_token.FirstOrDefault().Key,eventmodel.FromUserName);
+                        user.count = 0;
+                        UserBLL.UpdateUser(user);
+
+                    }
+                    resStr = WXMethdBLL.ResponseMsg(new Modal.WeiXinRequest.ContentRequest()
                     {
                         FromUserName = model.ToUserName,
                         ToUserName = model.FromUserName,
-                        Content = model.Event + ":" + model.EventKey ?? "没有key"
+                        Content = "感谢关注！回复任意消息可以获得定制二维码"
                     });
                     return Content(resStr);
                 }
                 else
                 {
+                    var bgpath = AppDomain.CurrentDomain.BaseDirectory + "\\img\\" + "backGroudImd.jpg";
+                    FileStream fs = new FileStream(bgpath, FileMode.Open);
+                    byte[] data = new byte[fs.Length];
+                    fs.Read(data, 0, data.Length);
+                    fs.Close();
+                    fs.Dispose();
+                    MemoryStream ms = new MemoryStream(data);
+
                     EventBase model = eventmodel as EventBase;
-                    var resStr = WXMethdBLL.ResponseMsg(new Modal.WeiXinRequest.ContentRequest()
+
+                    var ticket = QrcodeBLL.Get_QR_STR_SCENE_Qrcode(Access_token.FirstOrDefault().Key, model.FromUserName);
+                    var QrStream = QrcodeBLL.GetQrcodeStream(ticket);
+                    var user = UserBLL.GetUserInfo(model.FromUserName);
+
+                    var touxiangStream = UserBLL.GetTouxiang(user.headimgurl);
+
+                    var bg = ImgCom.ImgCommon.AddWaterPic(ms, touxiangStream, QrStream);
+
+                    //MediaBLL.UploadMultimedia()
+
+                    resStr = WXMethdBLL.ResponseMsg(new Modal.WeiXinRequest.ContentRequest()
                     {
                         FromUserName = model.ToUserName,
                         ToUserName = model.FromUserName,
                         Content = "hi"
                     });
-                    return Content(resStr);
                 }
+                return Content(resStr);
+
             }
 
 
-            //var bgpath = AppDomain.CurrentDomain.BaseDirectory + "\\img\\" + "backGroudImd.jpg";
-            //FileStream fs = new FileStream(bgpath, FileMode.Open);
-            //byte[] data = new byte[fs.Length];
-            //fs.Read(data, 0, data.Length);
-            //fs.Close();
-            //fs.Dispose();
-            //MemoryStream ms = new MemoryStream(data);
+
 
             //var touXiangPath = AppDomain.CurrentDomain.BaseDirectory + "\\img\\" + "touxiang.jpg";
             //FileStream fsTouXiang = new FileStream(touXiangPath, FileMode.Open);
@@ -79,7 +114,7 @@ namespace WXProjectWeb.Controllers
 
 
             //var outsteam = ImgCom.ImgCommon.AddWaterPic(ms, mstouxiang, mserweima, "张辉", "测试内容就是这样");
-            
+
 
             //return File(outsteam, "image/jpeg");
 
@@ -169,10 +204,10 @@ namespace WXProjectWeb.Controllers
             var outsteam = ImgCom.ImgCommon.AddWaterPic(ms, mstouxiang, mserweima, "张辉", "测试内容就是这样");
 
             ///上传图片
-            string media_id = MediaBLL.UploadMultimedia(token, "image","hui.jpg", outsteam);
+            string media_id = MediaBLL.UploadMultimedia(token, "image", "hui.jpg", outsteam);
 
             ///获取临时素材
-            string path = HttpContext.Server.MapPath("~/img/" );
+            string path = HttpContext.Server.MapPath("~/img/");
             MediaBLL.GetMultimedia(token, media_id, path);
 
             return Content(media_id);
