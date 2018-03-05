8a6d4b13edb8ebf75e4ef10c5bb85b963484bf61
jf8283062 <1006200300@qq.com>
2018-03-05 15:47:05 +0800

    shareCount

---
+++ WXProject/Modal/ShareCount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modal
+{
+    [Table("UserInfo")]
+    public class ShareCount
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public string openid { set; get; }
+        /// <summary>
+        /// 项目
+        /// </summary>
+        public string type { set; get; }
+        /// <summary>
+        /// 数量
+        /// </summary>
+        public int count { set; get; }
+
+    }
+}
