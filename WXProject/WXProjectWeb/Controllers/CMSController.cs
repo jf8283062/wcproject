ce6f7e4aa5cec76169f3563fa4880b8005fc77d0
jf8283062 <1006200300@qq.com>
2018-04-24 15:33:32 +0800

    框架

---
+++ WXProject/WXProjectWeb/Controllers/CMSController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WXProjectWeb.Controllers
+{
+    public class CMSController : Controller
+    {
+        public ActionResult Login()
+        {
+            ViewBag.HasCookies = Request.Cookies["user"] != null ;
+            return View();
+        }
+        public JsonResult PostLogin(string username, string password, string remeber)
+        {
+            if (username == "admin" && password == "admin")
+            {
+                Response.Cookies.Add(new HttpCookie("user", "admin"));
+            }           
+            return Json(new
+            {
+                success = true
+            });
+        }
+        // GET: CMS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
