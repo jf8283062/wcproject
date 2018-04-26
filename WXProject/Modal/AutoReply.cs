cafd543160f0900edb90e4c62cbf61ed4e713d94
saviourzhang <278314833@qq.com>
2018-04-26 09:24:55 +0800

    1

---
+++ WXProject/Modal/AutoReply.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Modal
+{
+    /// <summary>
+    /// 自动回复消息
+    /// </summary>
+    [Table("AutoReply")]
+    public class AutoReply
+    {
+        public int ID { get; set; }
+
+        [StringLength(500)]
+        public string Question { get; set; }
+
+        [StringLength(1000)]
+        public string ReplyContent { get; set; }
+
+        [StringLength(200)]
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+
+        [StringLength(200)]
+        public string ModifiedBy { get; set; }
+
+        public DateTime ModifiedTime { get; set; }
+    }
+}
---
+++ WXProject/WXProjectWeb/wcApi/AutoReplyBLL.cs
@@ -0,0 +1,56 @@
+﻿using Modal;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Data.Entity;
+using System.IO;
+using Modal;
+
+namespace WXProjectWeb.wcApi
+{
+    public class AutoReplyBLL
+    {
+
+
+        /// <summary>
+        /// 获取回复内容信息
+        /// </summary>
+        /// <returns></returns>
+        public static List<AutoReply> GetBasicServiceInfo()
+        {
+            List<AutoReply> list = new List<AutoReply>();
+            list.Add(new AutoReply() { ID = 1, Question = "1", ReplyContent = "1111" });
+            return list;
+        }
+
+        /// <summary>
+        /// 更新回复内容
+        /// </summary>
+        /// <param name="bs"></param>
+        /// <returns></returns>
+        public static int UpdateAutoReplyInfo(Modal.AutoReply bs)
+        {
+            //return dal.UpdateBasicServiceInfo(bs);
+           // return SimpleHelp.Update<T_BasicServiceInfo>(bs);
+            return 1;
+        }
+
+        /// <summary>
+        /// 添加回复内容
+        /// </summary>
+        /// <param name="bs"></param>
+        /// <returns></returns>
+        public static AutoReply AddNewAutoReplyInfo(AutoReply bs)
+        {
+            //return dal.AddNewBasicServiceInfo(bs);
+            // return SimpleHelp.Add(bs);
+
+            return new AutoReply() { ID = 2, Question = "2", ReplyContent = "222" };
+        }
+    }
+}
